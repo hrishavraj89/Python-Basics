5be44d98d8b189b8a59622c08a0c6f39c0a5648f
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-10 20:31:01 +0530

    Make some changes in the recursion file

--- chapter_8/recursion.py
+++ chapter_8/recursion.py
@@ -11,12 +11,3 @@ def factorial(n):
 
 n = int(input("Enter a number:- "))
 print(f"The factorial of the given number is {factorial(n)}")
-
-# def factorial(n):
-#     if n == 0 or n == 1:
-#         return 1
-#     else:
-#         return n * factorial(n - 1)
-
-# n = int(input("Enter a number:- "))
-# print(f"The factorial of the given number is {factorial(n)}")
