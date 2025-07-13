3d55c4c70491acafab60fc4d62ec9a95b341e949
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-13 21:29:22 +0530

    solved Problem 4

---
+++ chapter9/prob4.py
@@ -0,0 +1,11 @@
+# A file contains a word “Donkey” multiple times. You need to write a program
+# which replace this word with ##### by updating the same file. 
+
+word = "donkey"
+with open("donkey.txt", "r") as f:
+    content = f.read()
+
+New_content = content.replace("donkey", "######")
+
+with open("donkey.txt", "w") as f:
+    f.write(New_content)
