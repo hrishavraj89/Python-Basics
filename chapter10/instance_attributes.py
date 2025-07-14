b9836cb3449bd0b740d4b93dd3a30f15f341909b
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-14 22:17:13 +0530

    Learned Self Parameter

--- chapter10/instance_attributes.py
+++ chapter10/instance_attributes.py
@@ -1,7 +1,9 @@
 class Employee:
-    company = "Google" # Specific to Each Class
+    company = "Google" # Specific to Each Class(class attribute)
 harry = Employee() # Object Instatiation
-harry.company = "Youtube"  #Instance attribute
-print(harry.company)
+harry.company = "YouTube"   #Instance attribute
+harry.age = "24"   #Instance attribute
+print(harry.company, harry.age)
 
 # Note:- Instance attributes take preference over the class attribute
+# Here company is a class attribute and age is instance attribute
---
+++ chapter10/self_parameter.py
@@ -0,0 +1,8 @@
+class Employee:
+    company = "Google"
+    def getsalary(self):
+        print("Salary is not there")
+
+harry = Employee()
+
+harry.getsalary()
