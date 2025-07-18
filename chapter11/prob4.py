4c9a42e9d1b66e41be1a904f79694dbdb25e64db
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-18 22:39:17 +0530

    Solved problem 4

---
+++ chapter11/prob4.py
@@ -0,0 +1,15 @@
+class Complex:
+    def __init__(self, real_part, img_part):
+        self.real_part = real_part
+        self.img_part = img_part
+
+    def __add__(self, sum):
+        return Complex(self.real_part + sum.real_part, self.img_part + sum.img_part)
+
+    def __str__(self):
+        return f"{self.real_part} + {self.img_part}i"
+    
+a = Complex(1, 2)
+b = Complex(3, 4)
+result = a+b
+print(result)
