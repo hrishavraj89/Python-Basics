879bd47358d1af3b81dd734f947cca5e48008c8c
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-21 23:30:25 +0530

    Solve problem 6

---
+++ chapter13/prob6.py
@@ -0,0 +1,11 @@
+# Run pip freeze for the system interpreter. Take the contents and create a similar
+# virtualenv.
+
+# To start a fresh virtual env we use:-  python3 -m venv (your virtual env name)
+# To activate it, we use:- cd (your env name)  -->  cd Scripts  --> activate
+# To deactivate, simply type:- Deactivate
+
+
+# To create reqirements.txt:- pip freeze > requirements.txt
+
+# To install reqirements.txt:- pip install -r ../env1/requirements.txt
